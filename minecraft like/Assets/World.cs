0579460956e523a580173c10f6afcf8c90063ac7
Oneloves <43654542+Oneloves@users.noreply.github.com>
2018-09-29 13:14:07 +0200

    Build chunks in the world

--- minecraft like/Assets/World.cs
+++ minecraft like/Assets/World.cs
@@ -8,6 +8,7 @@ public class World : MonoBehaviour
     public Material textureAtlas;
     public static int columnHeight = 2;
     public static int chunkSize = 8;
+    public static int worldSize = 2;
     public static Dictionary<string, Chunk> chunks;
 
     public static string BuildChunkName(Vector3 v)
@@ -33,13 +34,32 @@ public class World : MonoBehaviour
 
     }
 
+    IEnumerator BuildWorld()
+    {
+        for (int z = 0; z < worldSize; z++)
+            for(int y = 0; y < worldSize; y++)
+                for(int x = 0; x < worldSize; x++)
+                {
+                    Vector3 chunkPosition = new Vector3(x * chunkSize, y * chunkSize, z * chunkSize);
+                    Chunk c = new Chunk(chunkPosition, textureAtlas);
+                    c.chunk.transform.parent = this.transform;
+                    chunks.Add(c.chunk.name, c);
+                }
+
+        foreach(KeyValuePair<string, Chunk> c in chunks)
+        {
+            c.Value.DrawChunk();
+            yield return null;            
+        }
+    }
+
     // Use this for initialization
     void Start()
     {
         chunks = new Dictionary<string, Chunk>();
         this.transform.position = Vector3.zero;
         this.transform.rotation = Quaternion.identity;
-        StartCoroutine(BuildChunkColumn());
+        StartCoroutine(BuildWorld());
     }
 
     // Update is called once per frame
